6486f1ec82f48753da01e9279c3c9adaa324dbb9
Owner <Owner@660084-COMPN-09>
2025-02-06 10:19:04 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Circle
+    {
+        protected Point center;
+        protected int radius;
+        public Circle(Point center, int radius)
+        {
+            this.center = center;
+            this.radius = radius;
+        }
+        public Circle(int x , int y, int radius)
+        {
+            this.center =  new Point(x, y);
+            this.radius = radius;
+        }
+        public Point GetCenter()
+        {
+            return this.center;
+        }
+        public void SetCenter(Point center)
+        {
+            this.center = center;
+        }
+
+        public int GetRadius()
+        {
+            return this.radius;
+        }
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(this.radius, 2);
+        }
+        public override string ToString()
+        {
+            return $"center {base.ToString()}, raduys: {this.radius}";
+        }
+
+    }
+}
